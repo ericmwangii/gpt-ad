9fcfd8803b01ddb4016b0c6d2fc337d0ca92a4ff
ericmwangii <ericmwangii101@gmail.com>
2022-02-09 09:21:47 +0300

    cors --dev

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const openai = new OpenAIApi(configuration);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(cors());
+app.use(cors("*"));
 
 app.get("/ad", async (req, res) => {
   let product = req.query.product;
